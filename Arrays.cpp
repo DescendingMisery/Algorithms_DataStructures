e5959d7f57ddc2897bde1323352b79e78788e04d
AbsolutelyBeginner <huy.businesscontact@gmail.com>
2023-06-28 10:40:11 -0500

    Add project files.

---
+++ Arrays.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main()
+{
+	int A[5] = { 1,3,4,5,6 };
+
+
+	cout << sizeof(A) << endl; //sizeof() returns the size of a type in bytes, so an int is 4 bytes, the array contains 5 elements so 4 x 5 = 20.
+	cout << A[4] << endl;
+	printf("%d\n", A[2]);
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << A[i] << endl;
+	}
+	return 0;
+}
