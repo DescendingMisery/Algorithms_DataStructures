ca36c3a6e7231bc54170eb64d1c1692703b91b17
AbsolutelyBeginner <huy.businesscontact@gmail.com>
2023-06-30 14:03:00 -0500

    Reference.cpp added

---
+++ Reference.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+/*
+int main()
+{
+	int a = 10;
+	int& r = a;
+	int b = 30;
+	r = b; // because r references a, so when this assignment happens, r changes value, so will a 
+
+	printf("%i", r);
+	return 0;
+}
+*/
